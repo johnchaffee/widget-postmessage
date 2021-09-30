1a634981d1ed0476d9499167f48cce7b882d9a43
John Chaffee <johnchaffee@mac.com>
2021-09-30 09:20:44 -0700

    Don't nest window event listener in window.onload function

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 // --- RECEIVING MESSAGEES ---
-window.onload = function () {
-  console.log("ONLOAD");
-
-  // // Setup event listener that logs when parent window receives message from child iframe
-  window.addEventListener("message", (event) => {
-    console.log(event);
-    const { data = {} } = event;  // event.data
-    const { type = "" } = data;  // data.type
-    if (type === "sendMessage") {
-      console.log("sendMessage:")
-      console.log(data);
-      alert("I got this message:\n" + JSON.stringify(event.data, null, 2));
-    }
-    if (type === "unreadCount") {
-      console.log("unreadCount:")
-      console.log(data);
-    }
-  });
-};
+// // Setup event listener that logs when parent window receives message from child iframe
+window.addEventListener("message", (event) => {
+  console.log(event);
+  const { data = {} } = event; // event.data
+  const { type = "" } = data; // data.type
+  if (type === "sendMessage") {
+    console.log("sendMessage:");
+    console.log(data);
+    alert("A message has been sent:\n" + JSON.stringify(event.data, null, 2));
+  }
+  if (type === "unreadCount") {
+    console.log("unreadCount:");
+    console.log(data);
+    alert("The unread count may have changed:\n" + JSON.stringify(event.data, null, 2));
+  }
+});
